2c8548c78cfbc49d604aead5251a9287170d0d56
ladydior89 <166581416+ladydior89@users.noreply.github.com>
2024-08-08 01:07:17 +0900

    Add files via upload
    PHP基礎　kadai_007 連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列を作成
+        $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+
+        // 連想配列を出力
+        print_r($vegetable);
+        ?>
+
+
+    </p>
+
+</body>
+
+</html>
